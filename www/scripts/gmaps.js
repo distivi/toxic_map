d913f66db7cf6a71f8660fc39751dacf748c9546
Stos Yura <100sbsh@gmail.com>
2014-04-02 14:02:55 +0300

    Added scalable vector graphic marker

--- www/scripts/gmaps.js
+++ www/scripts/gmaps.js
@@ -6,8 +6,18 @@ function initialize() {
     var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
 
+    var goldStar = {
+        path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+        fillColor: 'yellow',
+        fillOpacity: 0.8,
+        scale: 1,
+        strokeColor: 'gold',
+        strokeWeight: 14
+    };
+
     var marker = new google.maps.Marker({
         position: map.getCenter(),
+        icon : goldStar,
         map: map,
         title: 'Click to zoom'
     });
@@ -16,7 +26,7 @@ function initialize() {
     // 3 seconds after the center of the map has changed, pan back to the
     // marker.
     window.setTimeout(function() {
-        map.panTo(marker.getPosition());
+        map.goldStar(marker.getPosition());
         }, 3000);
     });
 }
