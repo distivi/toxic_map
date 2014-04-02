e43f997eb98304cba5631e3eab5a47a2ea82f2df
Dimediuk Stas <dymedyuk@gmail.com>
2014-04-02 22:41:33 +0300

    fixed marker position issue

--- www/scripts/gmaps.js
+++ www/scripts/gmaps.js
@@ -1,34 +1,49 @@
 function initialize() {
     var mapOptions = {
-    center: new google.maps.LatLng(46.947534, 32.055607),
-    zoom: 8
+        center: new google.maps.LatLng(46.947534, 32.055607),
+        zoom: 8
     };
+
     var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
 
     var goldStar = {
-        path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+        // path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+        path: 'M -20,-20 20,-20 20,20 -20,20 z',
         fillColor: 'yellow',
         fillOpacity: 0.8,
         scale: 1,
         strokeColor: 'gold',
-        strokeWeight: 14
+        strokeWeight: 0
     };
 
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        icon : goldStar,
-        map: map,
-        title: 'Click to zoom'
+   var markerImage = new google.maps.MarkerImage(
+        'http://icons.iconarchive.com/icons/hopstarter/sleek-xp-software/256/Yahoo-Messenger-icon.png',
+        new google.maps.Size(256,256),
+        new google.maps.Point(0,0),
+        new google.maps.Point(128,128)
+    );
+
+    google.maps.event.addListener(map, 'click', function(event) {
+       placeMarker(event.latLng);
     });
 
-    google.maps.event.addListener(map, 'center_changed', function() {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(function() {
-        map.goldStar(marker.getPosition());
-        }, 3000);
-    });
+    function placeMarker(location) {
+        var marker = new google.maps.Marker({
+            position: location, 
+            map: map,
+        });
+
+        // var marker2 = new google.maps.Marker({
+        //     position: location, 
+        //     icon : markerImage,
+        //     map: map
+        // });
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
