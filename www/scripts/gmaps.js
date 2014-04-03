3f2cb0be6cb96ce83869c1aca5296b9b4f5f4ae0
Dimediuk Stas <dymedyuk@gmail.com>
2014-04-03 06:17:50 +0300

    Added green circle to map

--- www/scripts/gmaps.js
+++ www/scripts/gmaps.js
@@ -17,33 +17,29 @@ function initialize() {
         strokeWeight: 0
     };
 
-   var markerImage = new google.maps.MarkerImage(
-        'http://icons.iconarchive.com/icons/hopstarter/sleek-xp-software/256/Yahoo-Messenger-icon.png',
-        new google.maps.Size(256,256),
-        new google.maps.Point(0,0),
-        new google.maps.Point(128,128)
-    );
-
     google.maps.event.addListener(map, 'click', function(event) {
        placeMarker(event.latLng);
     });
+
+    function getCircle(magnitude) {
+        console.log(magnitude);
+      return {
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: 'green',
+        fillOpacity: .2,
+        scale: magnitude,//Math.pow(2, magnitude) / Math.PI,
+        strokeColor: 'white',
+        strokeWeight: .5
+      };
+    }
 
     function placeMarker(location) {
         var marker = new google.maps.Marker({
             position: location, 
             map: map,
+            icon: getCircle(100)
         });
-
-        // var marker2 = new google.maps.Marker({
-        //     position: location, 
-        //     icon : markerImage,
-        //     map: map
-        // });
     }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
